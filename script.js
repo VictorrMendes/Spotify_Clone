a623f5bd8185531c6a59afc30a9eb7a23f4c94f9
VictorrMendes <victo.mendes.souza@gmail.com>
2025-02-02 22:58:19 -0300

    criando logica

--- script.js
+++ script.js
@@ -2,24 +2,44 @@ const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
-
-function requestApi(searchTerm){
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+function requestApi(searchTerm) {
+    // Requisição para pegar todos os artistas, sem filtro no backend
+    const url = `http://localhost:3000/artists`;
     fetch(url)
-        .then((Response) => Response.json())
-        // .then((result) => displayResults(result))
+        .then((response) => response.json())
+        .then((result) => displayResults(result, searchTerm));
 }
 
-function displayResults(){
-    return;
-}
+function displayResults(result, searchTerm) {
+    resultPlaylist.classList.add("hidden");
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
 
-document.addEventListener('input', function() {
-    const searchTerm = searchInput.value.toLowerCase()
-     if(searchTerm === ''){
+    // Filtrando os artistas que possuem o nome correspondente
+    const filteredResults = result.filter(artist =>
+        artist.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    if (filteredResults.length > 0) {
+        filteredResults.forEach(element => {
+            artistName.innerText = element.name;
+            artistImage.src = element.urlImg;
+        });
+        resultArtist.classList.remove('hidden');
+    } else {
         resultArtist.classList.add('hidden');
-        resultArtist.classList.remove('hidden')
+    }
+}
+
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value;
+
+    // Verificando se o campo de pesquisa está vazio
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
         return;
-     }
-     requestApi(searchTerm);
-})
+    }
+
+    requestApi(searchTerm);
+});
